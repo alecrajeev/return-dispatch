1fc30c43bdc8ca371a21cc5dbecc1fa2669810e2
alecrajeev <13004609+alecrajeev@users.noreply.github.com>
2025-12-18 20:14:11 -0500

    test 200

--- dist/index.mjs
+++ dist/index.mjs
@@ -24067,9 +24067,9 @@ async function dispatchWorkflow(distinctId) {
         distinct_id: distinctId
       }
     });
-    if (!((response.status == 204) || (response == 200))) {
+    if (response.status !== 200) {
       throw new Error(
-        `Failed to dispatch action, expected 204 but received ${response.status}`
+        `Failed to dispatch action, expected 200 but received ${response.status}`
       );
     }
     core3.info(
